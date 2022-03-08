14bd5adb1c7bafd43454342813f048f4b8bf0506
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 16:03:50 -0300

    Add project files.

---
+++ ExercicioOito.ConsoleApp/LataDeAzeite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioOito.ConsoleApp
+{
+    internal class LataDeAzeite
+    {
+        public double altura;
+        public double raio;
+        public double volume;
+
+        const double pi = 3.14;
+
+        public double CalcularVolumeLataAzeite(double altura, double raio)
+        {
+            volume = pi * (raio * raio) * altura;
+            return volume;
+        }
+    }
+}
---
+++ ExercicioOito.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioOito.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LataDeAzeite soya = new LataDeAzeite();
+            Console.Write("Informe a altura do cilindro: \n> ");
+            soya.altura = double.Parse(Console.ReadLine());
+            Console.Write("Informe o raio do cilindro: \n> ");
+            soya.raio = double.Parse(Console.ReadLine());
+            soya.volume = soya.CalcularVolumeLataAzeite(soya.altura, soya.raio);
+
+            Console.WriteLine("O volume desta belíssima lata de azeite é de: " + soya.volume.ToString("#.##") + "m³");
+            Console.ReadKey();
+        }
+    }
+}
